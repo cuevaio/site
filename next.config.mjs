9e303fb9b011fb74cac8da83b070fbb6151a1876
Anthony Cueva <hi@cueva.io>
2026-03-15 00:45:15 -0500

    feat: add redirect from /betterwriter to TestFlight

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
- 
-}
+	async redirects() {
+		return [
+			{
+				source: '/betterwriter',
+				destination: 'https://testflight.apple.com/join/EhmQk8U8',
+				permanent: false,
+			},
+		];
+	},
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	images: {
+		unoptimized: true,
+	},
+	experimental: {
+		nextScriptWorkers: true,
+	},
+};
 
-export default nextConfig
+export default nextConfig;
